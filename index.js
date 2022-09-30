2dbb66686f807e5857d827159a1826a16eed1292
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-09-30 13:15:36 +0300

    Edited: print section

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ import { print } from './js/lib.js';
 // Define your objects here
 
 class Inhabitant {
-   constructor(species, name, gender,saying) {
+   constructor(species, name, gender, saying) {
       this.species = species;
       this.name = name;
       this.gender = gender;
       this.saying = saying;
+   }
+   printOut() {
+      return ['species', 'name', 'gender', 'saying'].map(
+         (prop) => `${prop}: ${this[prop]}`
+      ).join('; ');
    }
 }
 
@@ -16,6 +21,12 @@ class Animal extends Inhabitant {
    constructor (species, name, gender, saying) {
       super (species, name, gender, saying);
       this.legs = 4;
+   }
+   printOut() {
+      return [
+         super.printOut(),
+         ...['legs'].map((prop) => `${prop}: ${this[prop]}`),
+      ].join('; ');
    }
 }
 
@@ -36,6 +47,12 @@ class Human extends Inhabitant {
       super('human', name, gender, saying);
       this.hands = 2;
       this.legs = 2;
+   }
+   printOut() {
+      return [
+         super.printOut(),
+         ...['hands','legs'].map((prop) => `${prop}: ${this[prop]}`),
+      ].join('; ');
    }
 }
 
@@ -76,8 +93,5 @@ const john = new Man ('John', 'Hi, Mary!');
    */
 
 const inhabitants = [toby, alice, mary, john];
-const props = ['species', 'name', 'gender', 'legs', 'hands', 'saying'];
-   
-inhabitants.map((item) => {
-   print(props.map((prop) => item[prop]).join('; '));
-});
+
+inhabitants.forEach(inhabitant => print(inhabitant.printOut()));
