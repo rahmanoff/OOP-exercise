9bc67b0cb15602fb33c42c0bc0df8dc0722908dc
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-10-10 17:21:17 +0300

    Added: legs to Inhabitant

--- index.js
+++ index.js
@@ -4,74 +4,75 @@ import { print } from './js/lib.js';
 // Define your objects here
 
 class Inhabitant {
-   constructor(species, name, gender, saying) {
+   constructor(species, name, gender, legs, saying) {
       this.species = species;
       this.name = name;
       this.gender = gender;
+      this.legs = legs;
       this.saying = saying;
    }
    printOut() {
-      return ['species', 'name', 'gender', 'saying'].map(
+      return ['species', 'name', 'gender', 'legs', 'saying'].map(
          (prop) => `${prop}: ${this[prop]}`
       ).join('; ');
    }
 }
 
 class Animal extends Inhabitant {
-   constructor (species, name, gender, saying) {
-      super (species, name, gender, saying);
+   constructor(species, name, gender, legs, saying) {
+      super(species, name, gender, legs, saying);
       this.legs = 4;
    }
    printOut() {
       return [
          super.printOut(),
-         ...['legs'].map((prop) => `${prop}: ${this[prop]}`),
+         ...[].map((prop) => `${prop}: ${this[prop]}`),
       ].join('; ');
    }
 }
 
 class Dog extends Animal {
-   constructor (name, gender, saying) {
-      super ('dog', name, gender, saying);
+   constructor(name, gender, saying) {
+      super('dog', name, gender, saying);
    }
 }
 
 class Cat extends Animal {
-   constructor (name, gender, saying) {
-      super ('cat', name, gender, saying);
+   constructor(name, gender, saying) {
+      super('cat', name, gender, saying);
    }
 }
 
 class Human extends Inhabitant {
-   constructor (name, gender, saying) {
-      super('human', name, gender, saying);
-      this.hands = 2;
+   constructor(name, gender, legs, hands, saying) {
+      super('human', name, gender, legs, hands, saying);
       this.legs = 2;
+      this.hands = 2;
    }
+
    printOut() {
-      return [
-         super.printOut(),
-         ...['hands','legs'].map((prop) => `${prop}: ${this[prop]}`),
-      ].join('; ');
+      return ['species', 'name', 'gender', 'legs', 'hands', 'saying'].map(
+         (prop) => `${prop}: ${this[prop]}`
+      ).join('; ');
    }
 }
 
 class Woman extends Human {
-   constructor (name, saying) {
-      super (name, 'female', saying);
+   constructor(name, saying) {
+      super(name, 'female', saying);
    }
 }
 
 class Man extends Human {
-   constructor (name, saying) {
-      super (name, 'male', saying);
+   constructor(name, saying) {
+      super(name, 'male', saying);
    }
 }
 
-const toby = new Dog ('Toby', 'male', 'woof-woof');
-const alice = new Cat ('Alice', 'female', 'meow');
-const mary = new Woman ('Mary', 'Hi, John!');
-const john = new Man ('John', 'Hi, Mary!');
+const toby = new Dog('Toby', 'male', 'woof-woof');
+const alice = new Cat('Alice', 'female', 'meow');
+const mary = new Woman('Mary', 'Hi, John!');
+const john = new Man('John', 'Hi, Mary!');
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
